37b7b528ec87f098882d62dad9859723ee525d8c
Shubham saini <110394904+shubham-saini18@users.noreply.github.com>
2026-06-24 15:49:07 +0530

    Add outputs for master and worker public IPs

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,1 +1,9 @@
+output "master_public_ip" {
+  value       = aws_instance.master.public_ip
+  description = "Public IP address of the Master Node"
+}
 
+output "worker_public_ips" {
+  value       = aws_instance.workers[*].public_ip
+  description = "Public IP addresses of the Worker Nodes"
+}
